c825b8ef0f14f8d72d165a021bfd919d363b104f
jonassvalin <jonassvalin@gmail.com>
2024-06-04 16:45:16 +0200

    Remove IAM user and add descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,24 @@
 output "ses_domain_identity_arn" {
   value = try(aws_ses_domain_identity.ses_domain[0].arn, "")
+  description = "The ARN of the SES domain identity"
 }
 
 output "ses_domain_identity_verification_token" {
   value = try(aws_ses_domain_identity.ses_domain[0].verification_token, "")
+  description = "A code which when added to the domain as a TXT record will signal to SES that the owner of the domain has authorised SES to act on their behalf. The domain identity will be in state 'verification pending' until this is done. See below for an example of how this might be achieved when the domain is hosted in Route 53 and managed by Terraform. Find out more about verifying domains in Amazon SES in the AWS SES docs."
 }
 
 output "ses_dkim_tokens" {
   value = try(aws_ses_domain_dkim.ses_domain_dkim.0.dkim_tokens, "")
+  description = "A list of DKIM Tokens which, when added to the DNS Domain as CNAME records, allows for receivers to verify that emails were indeed authorized by the domain owner."
 }
 
 output "spf_record" {
-  value = try(aws_route53_record.amazonses_spf_record[0].fqdn, "")
+  value = try(aws_route53_record.aws_ses_spf_record[0].fqdn, "")
+  description = "The SPF record for the domain. This is a TXT record that should be added to the domain's DNS settings to allow SES to send emails on behalf of the domain."
 }
 
-output "custom_from_domain" {
+output "mail_from_domain" {
   value = try(aws_ses_domain_mail_from.custom_mail_from[*].mail_from_domain, "")
-}
-
-output "ses_group_name" {
-  value = var.ses_group_name
-}
-
-output "ses_user" {
-  value = module.ses_user
+  description = "The custom MAIL FROM domain"
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,35 @@
 variable "domain" {
   type = string
+  description = "The domain to create the SES identity for."
 }
 
 variable "zone_id" {
   type    = string
+  description = "Route53 parent zone ID."
   default = ""
 }
 
 variable "verify_domain" {
   type    = bool
+  description = "If true the module will create Route53 DNS records used for domain verification."
   default = false
 }
 
 variable "verify_dkim" {
   type    = bool
+  description = "If true the module will create Route53 DNS records used for DKIM verification."
   default = false
 }
 
 variable "create_spf_record" {
   type    = bool
+  description = "If true the module will create an SPF record for `domain`."
   default = false
 }
 
 variable "mail_from_domain" {
   type     = string
+  description = "If provided the module will create a custom subdomain for the `MAIL FROM` address."
   default  = null
   nullable = true
 
@@ -31,27 +37,4 @@ variable "mail_from_domain" {
     condition     = var.mail_from_domain != null ? can(regex("^[a-zA-Z0-9-]+$", var.mail_from_domain)) : true
     error_message = "The mail_from_domain must be subdomain of the primary domain."
   }
-}
-
-variable "ses_group_enabled" {
-  type    = bool
-  default = true
-}
-
-variable "ses_group_name" {
-  type = string
-}
-
-variable "ses_group_path" {
-  type    = string
-  default = "/"
-}
-
-variable "ses_user_enabled" {
-  type    = bool
-  default = true
-}
-
-variable "ses_user_public_gpg_key_path" {
-  type = string
 }
