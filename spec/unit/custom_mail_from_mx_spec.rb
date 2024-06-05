2b08cf1e1984e049933566fafb64233efcbcddc9
belen-m-h <belen@go-atomic.io>
2024-06-05 11:30:10 +0100

    [#LGBK-158] Add custom mail from mx tests
    Co-authored-by: jonassvalin <jonassvalin@gmail.com>

---
+++ spec/unit/custom_mail_from_mx_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'custom mail from mx' do
+  let(:zone_id) do
+    var(role: :root, name: 'zone_id')
+  end
+
+  let(:region) do
+    var(role: :root, name: 'region')
+  end
+
+  describe 'by default' do
+    before(:context) do
+      @plan = plan(role: :root)
+    end
+
+    it 'does not create a custom mail from mx' do
+      expect(@plan)
+        .not_to(include_resource_creation(type: 'aws_route53_record')
+                  .once)
+    end
+  end
+
+  describe 'when mail from domain is provided' do
+    before(:context) do
+      @plan = plan(role: :root) do |vars|
+        vars.mail_from_domain = 'custom'
+      end
+    end
+
+    it 'creates a custom mail from mx' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_route53_record')
+              .once)
+    end
+
+    it 'has expected zone id' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_route53_record')
+              .with_attribute_value(:zone_id, zone_id))
+    end
+
+    it 'has expected type' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_route53_record')
+              .with_attribute_value(:type, 'MX'))
+    end
+
+    it 'has expected ttl' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_route53_record')
+              .with_attribute_value(:ttl, 600))
+    end
+
+    it 'has expected records' do
+      expect(@plan)
+        .to(include_resource_creation(type: 'aws_route53_record')
+              .with_attribute_value(
+                :records, ["10 feedback-smtp.#{region}.amazonses.com"]
+              ))
+    end
+  end
+end
